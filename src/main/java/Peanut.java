f8404bf1341333ce5193f91e840766dcbf7a0729
Marcus <marcusngzh@gmail.com>
2025-08-20 23:40:16 +0800

    Echo command of user and exits when bye is typed

--- src/main/java/Peanut.java
+++ src/main/java/Peanut.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Peanut {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,7 +10,22 @@ public class Peanut {
                 + "|____/ \\__,_|_|\\_\\___|\n";*/
         //System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Peanut\n" +
-                "What can I do for you?\n" +  "Bye. Hope to see you again soon!");
+                "What can I do for you?" );
+
+        while (true) {
+            String userInput = sc.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+
+
+            }
+            else {
+                System.out.println(userInput);
+            }
+        }
+
 
     }
 }
