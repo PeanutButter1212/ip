b987c99b8245d25f425108d9a5c9b2fcd82d6bc8
Marcus <marcusngzh@gmail.com>
2025-08-21 19:16:54 +0800

    Added tasklist to store tasks

--- src/main/java/Peanut.java
+++ src/main/java/Peanut.java
@@ -1,19 +1,11 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 public class Peanut {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<Task> tasks = new ArrayList<>();
-
-        /*String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";*/
-        //System.out.println("Hello from\n" + logo);
+        TaskList taskList = new TaskList();
         System.out.println("Hello! I'm Peanut\n" +
                 "What can I do for you?" );
 
@@ -26,54 +18,39 @@ public class Peanut {
 
             }
             else if(userInput.equals("list")) {
-                System.out.println("Here are the tasks in your list: ");
-                for (int i = 0 ; i < tasks.size()  ; i ++) {
-                    System.out.println((i+1) + ". " + tasks.get(i).toString());
-                }
+                taskList.list();
             }
 
             else if (userInput.startsWith("mark")) {
-                System.out.println("Nice! I've marked this task as done:");
                 String[] parts = userInput.split(" ", 2);
                 int taskNumber = Integer.parseInt(parts[1]) -1;
-                tasks.get(taskNumber).mark();
-                System.out.println(tasks.get(taskNumber).toString());
+                taskList.mark(taskNumber);
 
 
             }
             else if (userInput.startsWith("unmark")) {
-                System.out.println( "OK, I've marked this task as not done yet:");
                 String[] parts = userInput.split(" ", 2);
                 int taskNumber = Integer.parseInt(parts[1]) -1;
-                tasks.get(taskNumber).unmark();
-                System.out.println(tasks.get(taskNumber).toString());
-
-
+                taskList.unmark(taskNumber);
             }
             else if (userInput.startsWith("todo")) {
-                System.out.println("Got it. I've added this task:");
                 String[] parts= userInput.split("\\s+", 2);
-                String task = parts[1];
-                Task todoTask = new ToDo(task);
-                System.out.println(todoTask);
-                System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list");
-                tasks.add(todoTask);
+                Task todoTask = new ToDo(parts[1]);
+                System.out.println("Got it. I've added this task:");
+                taskList.add(todoTask);
             }
 
             else if (userInput.startsWith("deadline")) {
-                System.out.println("Got it. I've added this task:");
                 String[] parts = userInput.split("\\s+", 2);
                 String[] sep = parts[1].split("/by", 2);
                 String task = sep[0];
                 String deadline = sep[1];
                 Task deadlineTask = new Deadline(task, deadline );
-                System.out.println(deadlineTask);
-                System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list");
-                tasks.add(deadlineTask);
+                System.out.println("Got it. I've added this task:");
+                taskList.add(deadlineTask);
             }
 
             else if (userInput.startsWith("event")) {
-                System.out.println("Got it. I've added this task:");
                 String[] parts = userInput.split("\\s+", 2);
                 String[] sep = parts[1].split("/from", 2);
                 String[] sep2 = sep[1].split("/to",2);
@@ -81,14 +58,15 @@ public class Peanut {
                 String deadline = sep2[1];
                 String startDate = sep2[0];
                 Task eventTask = new Event(task, deadline,startDate );
-                System.out.println(eventTask);
-                System.out.println("Now you have " + (tasks.size() + 1) + " tasks in the list");
-                tasks.add(eventTask);
+                System.out.println("Got it. I've added this task:");
+                taskList.add(eventTask);
+
             }
 
             else {
+                Task genericTask = new Task(userInput);
                 System.out.println("added: " + userInput);
-                tasks.add(new Task(userInput));
+                taskList.add(genericTask);
             }
         }
 
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+        System.out.println(task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list");
+    }
+
+    public void list() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    public void mark(int index) {
+        tasks.get(index).mark();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(tasks.get(index));
+    }
+
+    public void unmark(int index) {
+        tasks.get(index).unmark();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(tasks.get(index));
+    }
+
+}
