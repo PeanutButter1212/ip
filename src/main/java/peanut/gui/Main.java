37f5120625a06e74502da3a6406c4af51572ddfc
Marcus <marcusngzh@gmail.com>
2025-09-17 16:17:12 +0800

    Add UserGuide

--- src/main/java/peanut/gui/Main.java
+++ src/main/java/peanut/gui/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            stage.setTitle("Peanut");
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(peanut); // inject the Duke instance
             stage.show();
